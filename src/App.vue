07457b82cc75524125f536fd83798434aacf3484
kkrisztian <kerteszkrisztian@turr.hu>
2023-03-02 11:39:12 +0100

    alapok

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+      <header>
+          <h1>
+              Gyroshop
+          </h1>
+      </header>
+</template>
+
+<style>
+header{
+    background-image: url('../src/assets/header_bgi.png');
+}
+</style>
