235d3498b90e23b3b6ff37cf6655e11b64279095
kkrisztian <kerteszkrisztian@turr.hu>
2023-03-16 12:58:25 +0100

    asd

--- src/views/etelek.vue
+++ src/views/etelek.vue
@@ -15,6 +15,43 @@
   </div>
 </div>
 
+<button class="btn btn-lg btn-dark m-3 felvetel " data-bs-toggle="modal" data-bs-target="#ujgyrosz"> <i class="bi bi-plus"></i></button>
+
+<div class="modal fade" id="ujgyrosz" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel"> Új Gyros fevétele </h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="input-group mb-3">
+          <span class="input-group-text"> Név </span>
+          <input type="text"  class="form-control" v-model="ujetel.nev">
+        </div>
+        <div class="input-group mb-3">
+          <span class="input-group-text"> Összetevők </span>
+          <input type="text" class="form-control" v-model="ujetel.osszetevok">
+        </div>
+        <div class="input-group mb-3">
+          <span class="input-group-text"> Ár </span>
+          <input type="number" class="form-control" v-model="ujetel.ar">
+        </div>
+        <div>
+          Kép majd lesz...
+        </div>
+        <div class="d-flex align-items-center align-self-center">
+          <button class="btn btn-dark m-3 " @click="UjGyros()"> Beküldés </button>
+        </div>
+        <div>
+
+        </div>
+      </div>
+      <button type="button" class="btn btn-danger" data-bs-dismiss="modal"> Bezár </button>
+      </div>
+  </div>
+</div>
+
 
 </template>
 
@@ -28,6 +65,7 @@ export default{
    data(){
       return{
          etelek: [],
+         ujetel:{},
          baseurl:"http://localhost/Gyros/API/database.php"
       }
    },
@@ -84,6 +122,25 @@ export default{
       console.log(err);
     })
   },
+  UjGyros(){
+    if (this.ujetel.nev == "" || this.ujetel.ar == "" || this.ujetel.osszetevok == null) {
+      alert("Minden mezőt kötelező kitölteni!")
+    }
+    else{
+      let data= {
+      table: "etelek",
+      values: {
+        nev: this.ujetel.nev,
+        ar: this.ujetel.ar,
+        osszetevok: this.ujetel.osszetevok
+      }
+    }
+    axios.post(this.baseurl, data).then(res => {
+      data.values.ID = res.insertId;
+
+    })
+    }
+  },
    }
   }
 
@@ -101,6 +158,11 @@ export default{
 .kep{
   object-fit:cover;
   height: 200px;
+}
+.felvetel{
+  position: fixed;
+  bottom: 0;
+  left: 0;
 }
 
 
